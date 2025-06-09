7c9a34f3e22780aaac38afa4d15d516ac020ea16
Cagatay <cagatay@guley.com.tr>
2025-06-09 15:34:18 +0300

    Update announce.js

--- public/announce.js
+++ public/announce.js
@@ -2,26 +2,8 @@
   const wsUrl =
     (location.protocol === "https:" ? "wss://" : "ws://") +
     "cdn.ore2b.com/parties/globe/default";
+
   try {
     const ws = new WebSocket(wsUrl);
-
-    ws.onopen = function() {
-      /*
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          ws.send(
-            JSON.stringify({
-              lat: pos.coords.latitude,
-              lng: pos.coords.longitude,
-              source: "ore2b.com"
-            })
-          );
-        });
-      }
-      */
-    };
-
-    ws.onerror = function() {};
-    ws.onclose = function() {};
   } catch (e) {}
 })();
