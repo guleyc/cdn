827364cccf450329a81ca7e6c2edb16af46ae14a
Cagatay <cagatay@guley.com.tr>
2025-06-09 15:22:13 +0300

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -106,11 +106,6 @@ function App() {
       />
 
       {/* Let's give some credit */}
-      <p>
-        Powered by <a href="https://cobe.vercel.app/">🌏 Cobe</a>,{" "}
-        <a href="https://www.npmjs.com/package/phenomenon">Phenomenon</a> and{" "}
-        <a href="https://npmjs.com/package/partyserver/">🎈 PartyServer</a>
-      </p>
     </div>
   );
 }
