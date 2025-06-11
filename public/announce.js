a1092eca4bd6f91f52bce518a96aa89cfa2873ba
guleyc <cagatay@guley.com.tr>
2025-06-11 15:42:00 +0300

    Update announce.js

--- public/announce.js
+++ public/announce.js
@@ -1,9 +1,24 @@
-(function() {
+(function () {
   const wsUrl =
     (location.protocol === "https:" ? "wss://" : "ws://") +
     "cdn.ore2b.com/parties/globe/default";
 
   try {
     const ws = new WebSocket(wsUrl);
-  } catch (e) {}
+
+    ws.onopen = function () {
+      console.log("WebSocket connection opened:", wsUrl);
+    };
+    ws.onmessage = function (event) {
+      console.log("Message received:", event.data);
+    };
+    ws.onerror = function (error) {
+      console.error("WebSocket error:", error);
+    };
+    ws.onclose = function () {
+      console.log("WebSocket connection closed.");
+    };
+  } catch (e) {
+    console.error("WebSocket connection error:", e);
+  }
 })();
